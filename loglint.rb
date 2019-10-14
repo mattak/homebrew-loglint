249fab426000311db8805fc34b1d2d47cf9d3e8c
mattak <mattak.me@gmail.com>
2019-10-15 02:32:48 +0900

    :up: Bump up v0.1.2

--- loglint.rb
+++ loglint.rb
@@ -1,8 +1,8 @@
 class Loglint < Formula
   desc "linter by analyzing build log"
   homepage "https://github.com/mattak/loglint"
-  url "https://github.com/mattak/loglint/archive/v0.1.1.tar.gz"
-  sha256 "dd8be0cb32ec1247f1c50f5e9f4526267bd6cb0d73ecdc06d167b00c0a24a553"
+  url "https://github.com/mattak/loglint/archive/v0.1.2.tar.gz"
+  sha256 "6ffc7ea140394b17032e669ea4698c9ccd90b25a1264e37e82328e1f7beb5caf"
 
   depends_on "go" => :build
   depends_on "make" => :build
